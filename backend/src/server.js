377da7e1a5640edf02f38b37d3f9fee718c668fb
Galen Alvian <galen@LAPTOP-GALEN.localdomain>
2026-03-15 19:56:13 +0700

    install express.js

---
+++ backend/src/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import routes from '../src/routes.js';
+
+const app = express();
+const port = 3000;
+const host = 'localhost';
+
+app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
+app.use('/', routes);
+
+app.listen(port, host, () => {
+  console.log(`Server berjalan di http://${host}:${port}`);
+});
